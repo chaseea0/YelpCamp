546b8daa1cbb0bcb9138a1172d75bd30120b7c87
Evan Chase <chaseea0@gmail.com>
2024-11-11 10:52:48 -0500

    feat(426): add logic to create new campgrounds
    - add route for a 'new campground' page
    - add route to post new campground info and save it to the db
    - create new campground creation page

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -23,6 +25,20 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    /* 
+    Below, we have to use req.body.campground (instead of just req.body) because we use 
+    the 'name' attribute in our new.ejs file to group our data together.
+    */
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground.id}`);
 })
 
 app.get('/campgrounds/:id', async (req, res) => {

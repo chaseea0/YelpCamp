f740ac3cf45d2aa0545d489458fa4db638100a3e
Evan Chase <chaseea0@gmail.com>
2024-11-20 15:39:20 -0500

    feat(442): update campground model to include image and description
    - adds description and image to the campground model
    - reseeds the database to include images and description
    - update the show page to show the image and description

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () => {
     await Campground.deleteMany({}); // deletes everything
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000); // picks a random location value
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${GetSample(descriptors)} ${GetSample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sed cumque illo debitis iusto ducimus! Perspiciatis architecto suscipit quae voluptatibus, non, vitae rerum repellat sapiente reiciendis libero hic deleniti. Laudantium! Perspiciatis cupiditate voluptatibus fugit deleniti exercitationem itaque excepturi, eveniet minus expedita sed, totam velit soluta fuga aliquam ea esse porro molestias deserunt voluptatum doloribus. Quae quibusdam mollitia harum doloremque officia. Error nostrum fugiat voluptatibus consequuntur iure, nisi harum aspernatur laudantium officiis corporis explicabo minima omnis repellendus. Iusto iste est animi mollitia rem sapiente laborum ex iure nobis odit, repellat voluptatibus!',
+            price
         })
         await camp.save();
     }

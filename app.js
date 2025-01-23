1ab1fd87521bf6fcc4e7620ff4631234207dec34
Evan Chase <chaseea0@gmail.com>
2025-01-23 16:07:47 -0500

    feat(484): add post route for adding reviews
    - add post route that pushes the new review onto the relevant campground and redirect to the same page
    - update logic on in show.ejs to have the form submission make a post request to the specified route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
 const { campgroundSchema } = require('./schemas');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -64,6 +65,15 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const id = req.params.id;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit', { campground });
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`);
 }))
 
 app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
